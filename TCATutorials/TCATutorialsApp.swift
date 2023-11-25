4e5126b04d199bf0d66b933a4cf05632e0253e9a
uhooi <the_uhooi@yahoo.co.jp>
2023-11-25 14:44:13 +0900

    Create project

---
+++ TCATutorials/TCATutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialsApp.swift
+//  TCATutorials
+//
+//  Created by uhooi on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
