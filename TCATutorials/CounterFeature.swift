7742ea17646fea749d9fd6d4cac378ef2d15eaaf
uhooi <the_uhooi@yahoo.co.jp>
2023-11-25 17:52:29 +0900

    Update CounterFeature

--- TCATutorials/CounterFeature.swift
+++ TCATutorials/CounterFeature.swift
@@ -8,12 +8,15 @@
 import ComposableArchitecture
 import SwiftUI
 
+// MARK: - Reducer
+
 @Reducer
 struct CounterFeature {
     struct State: Equatable {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
     
     enum Action {
@@ -21,7 +24,11 @@ struct CounterFeature {
         case factButtonTapped
         case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+    
+    enum CancelID { case timer }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -50,10 +57,31 @@ struct CounterFeature {
                 state.count += 1
                 state.fact = nil
                 return .none
+                
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
 }
+
+// MARK: - View
 
 struct CounterView: View {
     let store: StoreOf<CounterFeature>
@@ -84,6 +112,14 @@ struct CounterView: View {
                     .background(.black.opacity(0.1))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
+                
+                Button(viewStore.isTimerRunning ? "Stop timer" : "Start timer") {
+                    viewStore.send(.toggleTimerButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(.black.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
                 
                 Button("Fact") {
                     viewStore.send(.factButtonTapped)
